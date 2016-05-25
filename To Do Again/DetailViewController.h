4ef26870cb7fcb41633785d19f16fe08fc9451d0
RKing <rosalyn.kingsmill@gmail.com>
2016-05-25 11:25:18 -0400

    Initial Commit

---
+++ To Do Again/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  To Do Again
+//
+//  Created by Rosalyn Kingsmill on 2016-05-25.
+//  Copyright © 2016 Rosalyn Kingsmill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
